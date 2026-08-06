fc104fe452898bf96ad12b7acc55d855d3b16317
motasm20 <ps171179@edu.summacollege.nl>
2026-08-06 06:42:45 +0200

    Add generated files and assembly info for 'fluitende fietser' project
    - Created sourcelink.json for debugging support.
    - Added AssemblyInfo.cs and AssemblyInfoInputs.cache for assembly metadata.
    - Generated editorconfig for project settings.
    - Created GlobalUsings.g.cs for global using directives.
    - Added assets.cache for package references.
    - Generated markup compile cache and lref files for XAML files.
    - Added generated files for Afrekenen, MainWindow, and RekenMachine.
    - Updated project to target .NET 8.0 with Windows 7.0 as minimum platform.

--- wpf/fluitende fietser/obj/Debug/net8.0-windows/fluitende fietser.AssemblyInfo.cs
+++ wpf/fluitende fietser/obj/Debug/net8.0-windows/fluitende fietser.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fluitende fietser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db8693d3e26b049fcecc261bf88f3e23438636a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("fluitende fietser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fluitende fietser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
